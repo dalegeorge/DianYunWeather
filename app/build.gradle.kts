de757b35667a670f511da082e0b8a5f3db91271b
dalegeorge <jourtom3o2@gmail.com>
2025-12-14 23:25:45 +0800

    - 添加天气图标：晴天，雨天，多云 - 添加天气页面，主页面，菜单订单页面框架

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -11,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.weatherminiapp"
-        minSdk = 22
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -35,6 +36,10 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+        compose = true
+    }
 }
 
 dependencies {
@@ -43,7 +48,28 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    // 应用SQLite数据库依赖
+    implementation(files("./sqlite-android-3510100.aar"))
+    // 应用OkHttp网络框架
+    implementation("com.squareup.okhttp3:okhttp:5.3.2")
+    // 应用coil图片库处理框架
+    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
 }
---
+++ app/src/main/java/com/example/weatherminiapp/WeatherPage.kt
@@ -0,0 +1,4 @@
+package com.example.weatherminiapp
+
+class WeatherPage {
+}
